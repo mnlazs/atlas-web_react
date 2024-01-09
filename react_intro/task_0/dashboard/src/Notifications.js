e7712652608da7b8b93771c098b3be5f4dfd4223
mnlazs <mnl.azs@gmail.com>
2024-01-09 14:00:52 -0600

    files

---
+++ react_intro/task_0/dashboard/src/Notifications.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Notifications.css';
+
+
+function Notificacions(){
+  return (
+    <div className="Notifications">
+      <p> Here is the list of Notificacions</p>
+    </div>
+  )
+}
+
+export default Notificacions;
