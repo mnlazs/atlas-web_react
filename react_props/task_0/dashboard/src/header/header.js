10ac83e6003482c69edcde975a60d51ed67f390e
mnlazs <mnl.azs@gmail.com>
2024-01-10 08:55:54 -0600

    updated

---
+++ react_props/task_0/dashboard/src/header/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './atlas_logo.webp'; // Asegúrate de reemplazar 'path_to_holberton_logo.svg' con la ruta correcta al logo de Holberton
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+      </body>
+      <footer className="App-footer">
+      </footer>
+    </div>
+  );
+}
+
+export default App;
