a0ebfc3d027d3b8eeb1d8ae84ece1985dcbed176
mnlazs <mnl.azs@gmail.com>
2024-01-10 21:49:56 -0600

    updated

---
+++ react_props/task_4/dashboard/src/App/App.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from './App';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+
+describe('App Component Tests', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should contain the Notifications component', () => {
+    expect(wrapper.containsMatchingElement(<Notifications />)).toBeTruthy();
+  });
+
+  it('should contain the Header component', () => {
+    expect(wrapper.containsMatchingElement(<Header />)).toBeTruthy();
+  });
+
+  it('should contain the Login component', () => {
+    expect(wrapper.containsMatchingElement(<Login />)).toBeTruthy();
+  });
+
+  it('should contain the Footer component', () => {
+    expect(wrapper.containsMatchingElement(<Footer />)).toBeTruthy();
+  });
+});
---
+++ react_props/task_4/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './Footer'; 
+
+describe('Footer Component Tests', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Footer />);
+  });
+
+  it('should render without crashing', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('should render the text "Copyright"', () => {
+    expect(wrapper.text()).toContain('Copyright');
+  });
+});
