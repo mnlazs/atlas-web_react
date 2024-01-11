53f7dcc947c32ce74a5493e1040d37814d80fb0d
mnlazs <mnl.azs@gmail.com>
2024-01-11 10:18:09 -0600

    updated

---
+++ react_props/task_4/dashboard/src/config/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Rutas de los archivos de prueba o patrones a ignorar
+  testPathIgnorePatterns: ["/node_modules/"],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+
+  // Directorio donde Jest buscará los tests
+  roots: ["<rootDir>/src"],
+
+  // Soporte para importaciones de módulos
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+
+  // Configuración para transformar archivos con babel-jest
+  transform: {
+      "^.+\\.(js|jsx)$": "babel-jest"
+  },
+
+  // Configuración para el uso de `@testing-library/react`
+  setupFilesAfterEnv: [
+      "@testing-library/jest-dom/extend-expect"
+  ],
+
+  // Configuración de entorno de prueba para emular el navegador
+  testEnvironment: "jsdom"
+};
