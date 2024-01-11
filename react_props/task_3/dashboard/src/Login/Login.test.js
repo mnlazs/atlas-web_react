067d0399f6d3abe712661ab283c21813b88411f0
mnlazs <mnl.azs@gmail.com>
2024-01-10 18:15:17 -0600

    updated

---
+++ react_props/task_3/dashboard/src/Login/Login.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import Login from './Login';
+
+describe ('Login Component Test', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Login />);
+  });
+
+
+  it ('should render without crashing', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('should render 2 input tags and 2 label tags', () => {
+    expect(wrapper.find('input').length).toBe(2);
+    expect(wrapper.find('label').length).toBe(2);
+  });
+});
