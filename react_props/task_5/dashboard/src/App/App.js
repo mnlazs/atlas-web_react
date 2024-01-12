50872984517c6df7431c65c653407b83f77fccfe
mnlazs <mnl.azs@gmail.com>
2024-01-12 01:11:54 -0600

    updated

--- react_props/task_5/dashboard/src/App/App.js
+++ react_props/task_5/dashboard/src/App/App.js
@@ -6,6 +6,8 @@ import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+
 
 function App({ isLoggedIn }) {
   const [displayDrawer, setDisplayDrawer] = useState(false);
@@ -15,19 +17,19 @@ function App({ isLoggedIn }) {
   };
 
   // Crea el array listCourses
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 }
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
   ];
 
   return (
     <>
       <div className="menuItem" onClick={toggleDrawer}>Toggle Notifications</div>
-      {displayDrawer && <Notifications />}
+      <Notifications displayDrawer={displayDrawer} listNotifications={listNotifications} />
       <div className="App">
         <Header />
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        {isLoggedIn ? <CourseList /> : <Login />}
         <Footer />
       </div>
     </>
--- react_props/task_5/dashboard/src/Notifications/Notifications.js
+++ react_props/task_5/dashboard/src/Notifications/Notifications.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
-import { getLatestNotification } from '../utils/utils';
+import { NotificationItemShape } from './NotificationItemShape'; // Asegúrate de que la ruta es correcta
 import NotificationItem from './NotificationItem';
 
-function Notifications({ displayDrawer }) {
+function Notifications({ displayDrawer, listNotifications }) {
   const handleButtonClick = () => {
     console.log('Close button has been clicked');
   };
@@ -14,22 +14,24 @@ function Notifications({ displayDrawer }) {
     <>
       <div className="menuItem">
         <p>Your notifications</p>
-      </div> 
+      </div>
       {displayDrawer && (
         <div className="Notifications">
-          <button
-            style={{ float: 'right' }}
-            aria-label="Close"
-            onClick={handleButtonClick}
-          >
+          <button style={{ float: 'right' }} aria-label="Close" onClick={handleButtonClick}>
             <img src={closeIcon} alt="Close Icon" />
           </button>
-          <p>Here is the list of notifications:</p>
-          <ul>
-            <NotificationItem type="default" value="New course available" />
-            <NotificationItem type="urgent" value="New resume available" />
-            <NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
-          </ul>
+          {listNotifications.length === 0 ? (
+            <p>No new notification for now</p>
+          ) : (
+            <>
+              <p>Here is the list of notifications:</p>
+              <ul>
+                {listNotifications.map(notification => (
+                  <NotificationItem key={notification.id} {...notification} />
+                ))}
+              </ul>
+            </>
+          )}
         </div>
       )}
     </>
@@ -38,10 +40,12 @@ function Notifications({ displayDrawer }) {
 
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape)
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
+  listNotifications: []
 };
 
 export default Notifications;
