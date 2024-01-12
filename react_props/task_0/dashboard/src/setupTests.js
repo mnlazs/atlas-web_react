7c3f94b6116f962348b6bc2308df95f419ff9851
mnlazs <mnl.azs@gmail.com>
2024-01-12 09:13:30 -0600

    Updates

--- react_props/task_0/dashboard/src/setupTests.js
+++
@@ -1,4 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'; // Asegúrate de que el adaptador coincida con tu versión de React
-
-configure({ adapter: new Adapter() });
