7c83a8bdcaf4867ee7d125d00e84f890d243ef4a
mnlazs <mnl.azs@gmail.com>
2024-01-12 12:12:22 -0600

    update

---
+++ React_component/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from '../CourseList/CourseListRow';
+
+describe('<CourseListRow />', () => {
+  // Cuando isHeader es true
+  it('renderiza una celda con colspan = 2 cuando textSecondCell no existe', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" />);
+    const thElement = wrapper.find('th').first();
+    expect(thElement.exists()).toBe(true);
+    expect(thElement.props().colSpan).toBe(2);
+  });
+
+  it('renderiza dos celdas cuando textSecondCell está presente', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" textSecondCell="Second cell" />);
+    const thElements = wrapper.find('th');
+    expect(thElements).toHaveLength(2);
+  });
+
+  // Cuando isHeader es false
+  it('renderiza correctamente dos elementos td dentro de un elemento tr', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="First cell" textSecondCell="Second cell" />);
+    const tdElements = wrapper.find('td');
+    expect(tdElements).toHaveLength(2);
+  });
+});
