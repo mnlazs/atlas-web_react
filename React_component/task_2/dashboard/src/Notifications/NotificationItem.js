9299712d71d3167159a9add44bac581eff1e5d54
mnlazs <mnl.azs@gmail.com>
2024-01-12 12:33:36 -0600

    updated

---
+++ React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends Component {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    const listItemContent = html ? (
+      <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={() => markAsRead(id)}></li>
+    ) : (
+      <li data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>
+    );
+
+    return listItemContent;
+  }
+}
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+};
+
+export default NotificationItem;
