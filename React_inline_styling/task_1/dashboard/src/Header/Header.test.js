f2e766b4bcb48491f6ec5231665004417ad3c484
mnlazs <mnl.azs@gmail.com>
2024-01-16 16:41:22 -0600

    updated

--- React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import  { shallow } from 'enzyme';
 import Header from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
+
 
 describe('Header Component Test',() => {
   let wrapper;
 
   beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
     wrapper = shallow(<Header />);
   });
 
+  afterEach(() => {
+    // Reanuda la inyección de estilos después de cada prueba
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  
   it('should render without crashing', () => {
     expect(wrapper).toBeTruthy();
 });
