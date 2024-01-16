4739781f11925ae03137d368eb137e437ad180ef
mnlazs <mnl.azs@gmail.com>
2024-01-16 16:52:29 -0600

    updated

--- React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
+++ React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NotificationItem from './NotificationItem';
+import { StyleSheetTestUtils } from 'aphrodite';
+
 
 describe('<NotificationItem />', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppress
+    StyleInjection()
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    });
+    
+    
   it('calls markAsRead with the right ID when clicked', () => {
     const markAsReadSpy = jest.fn();
     const notificationId = 1; // Ejemplo de ID de notificación
