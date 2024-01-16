c8d45d5cef25deebaac7751ca5e6ec036256bea2
mnlazs <mnl.azs@gmail.com>
2024-01-16 11:41:50 -0600

    updated

---
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
+
+describe('<CourseListRow />', () => {
+  // Cuando isHeader es true
+  it('renderiza una celda con colspan = 2 cuando textSecondCell no existe', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" />);
+    const thElement = wrapper.find('th').first();
+    expect(thElement.exists()).toBe(true);
+    expect(thElement.prop('colSpan')).toBe(2);
+  });
+
+  it('renderiza dos celdas cuando textSecondCell está presente', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" textSecondCell="Second cell" />);
+    const thElements = wrapper.find('th');
+    expect(thElements).toHaveLength(2);
+  });
+
+  // Cuando isHeader es false
+  it('renderiza correctamente dos elementos td dentro de un elemento tr', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="First cell" textSecondCell="Second cell" />);
+    const tdElements = wrapper.find('td');
+    expect(tdElements).toHaveLength(2);
+  });
+  it('aplica el estilo de fondo correcto para las filas de encabezado', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" />);
+    expect(wrapper.find('tr').prop('style')).toHaveProperty('backgroundColor', '#deb5b545');
+  });
+
+  it('aplica el estilo de fondo correcto para las filas normales', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="First cell" />);
+    expect(wrapper.find('tr').prop('style')).toHaveProperty('backgroundColor', '#f5f5f5ab');
+  });
+});
