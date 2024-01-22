4e5e0b4a08b3caf8f32a7ad9f62409d7099385c8
mnlazs <mnl.azs@gmail.com>
2024-01-22 15:10:25 -0600

    updated header

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import AppContext from '../App/AppContext'; // Asegúrate de que la ruta de importación sea correcta
 
-const Header = () => {
-  return (
-    <div className={css(styles.header)}>
-      <img src="logo.png" alt="Logo" className={css(styles.logo)} />
-      <h1 className={css(styles.title)}>Dashboard</h1>
-    </div>
-  );
-};
+class Header extends Component {
+  static contextType = AppContext;
+
+  render() {
+    const { user, logOut } = this.context;
+
+    return (
+      <div>
+        <div className={css(styles.header)}>
+          <img src="logo.png" alt="Logo" className={css(styles.logo)} />
+          <h1 className={css(styles.title)}>Dashboard</h1>
+        </div>
+        {user.isLoggedIn && (
+          <div id="logoutSection" className={css(styles.logoutSection)}>
+            Welcome {user.email} (<span onClick={logOut} style={{cursor: 'pointer'}}>logout</span>)
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   header: {
@@ -25,6 +39,12 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: '24px',
+  },
+  logoutSection: {
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '10px',
+    textAlign: 'center',
   },
 });
 
