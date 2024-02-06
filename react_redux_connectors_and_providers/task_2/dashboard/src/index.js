710a9a521155545445b069be034b7779d853f3ca
mnlazs <mnl.azs@gmail.com>
2024-02-06 14:21:42 -0600

    updated

--- react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App/App';
-import store from './store'; // Esto importará el store desde './store/index.js'
+import store from './store'; 
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'; 
+import uiReducer from './reducers/uiReducer';
+const rootReducer = combineReducers({
+  uiReducer,
+});
+
+// Crea el store aplicando el middleware redux-thunk
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk) // Aplica el middleware redux-thunk
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +30,6 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
+export default store;
+export { loginRequest, logout };
